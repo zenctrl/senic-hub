8a704a54e094fa77553098369f377931a31d907b
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-01-31 17:16:39 +0100

    Display wifi password field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './App.css'
+import WifiPassword from './WifiPassword'
 import WifiSelection from './WifiSelection'
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className="App">
         <WifiSelection />
+        <WifiPassword ssid="WIFI-1" />
       </div>
     );
   }
---
+++ src/WifiPassword.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class WifiPassword extends Component {
+  render() {
+    return (
+      <div>
+        <div>Please enter your Wi-Fi password</div>
+        <div>Wi-Fi network: { this.props.ssid }</div>
+        <input type="password" />
+      </div>
+    )
+  }
+}
+
+export default WifiPassword
