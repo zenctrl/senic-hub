27c0e5fc8a272c8c1edda8e846bba9b818634506
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-02-01 16:47:56 +0100

    Post entered wifi password to api endpoint

--- src/WifiPassword.js
+++ src/WifiPassword.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
 class WifiPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { password: '' };
+  }
+
   render() {
     return (
       <div>
         <div>Please enter the password for { this.props.params.ssid }</div>
-        <input type="password" />
+        <input type="password" value={this.state.password} onChange={this.onPasswordChange.bind(this)} />
+        <input type="button" value="Continue" onClick={this.submitPassword.bind(this)}/>
       </div>
     )
+  }
+
+  onPasswordChange(event) {
+    this.setState({password: event.target.value})
+  }
+
+  submitPassword(event) {
+    let postData = new FormData()
+    postData.append('json', JSON.stringify({
+      'ssid':     this.props.params.ssid,
+      'password': this.state.password
+    }))
+    fetch('/-/setup/wifi', { method: 'POST', body: postData })
+      .then((response) => response.json())
+      .then((json) => console.log(json))
+    event.preventDefault()
   }
 }
 
