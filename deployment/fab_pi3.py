2da5772a2d94f0dd1a932812577cf6527da61968
Tom Lazar <tomster@pyfidelity.com>
2017-02-14 14:44:41 +0100

    Adapt bootstrapping to raspbian jessie

--- deployment/fab_pi3.py
+++ deployment/fab_pi3.py
@@ -2,19 +2,17 @@
 from os import path
 from fabric import api as fab
 from fabric.api import task, env
-from ploy.config import value_asbool
 
 eth_interface = """auto {eth_iface}
-iface {eth_iface} inet static
-  address {eth_ip}
-  netmask {eth_netmask}
-  gateway {eth_gateway}
-  dns-nameservers {eth_dns}
+interface {eth_iface}
+static ip_address={eth_ip}/{eth_netmask}
+static routers={eth_gateway}
+static domain_name_servers {eth_dns}
 """
 
 
 @task
-def bootstrap(boot_ip=None, authorized_keys='authorized_keys', static_ip=True):
+def bootstrap(boot_ip=None, authorized_keys='authorized_keys', static_ip=False):
     """bootstrap a freshly booted Raspberry PI 3 to make it ansible ready"""
     # (temporarily) set the user to `pi`
     if not path.isabs(authorized_keys):
@@ -26,26 +24,22 @@ def bootstrap(boot_ip=None, authorized_keys='authorized_keys', static_ip=True):
     if boot_ip:
         env.instance.config['ip'] = boot_ip
     hostname = env.instance.uid
-    env.host_string = 'ubuntu@%s' % hostname
+    env.host_string = 'pi@%s' % hostname
+    env.password = 'raspberry'
     AV = env.instance.get_ansible_variables()
     AV.setdefault('eth_ip', final_ip)
     AV.setdefault('eth_iface', 'eth0')
-    AV.setdefault('eth_netmask', '255.255.255.0')
+    AV.setdefault('eth_netmask', '24')
     AV.setdefault('eth_gateway', '192.168.1.1')
     AV.setdefault('eth_dns', '8.8.8.8')
     with fab.settings(warn_only=True):
-        if value_asbool(static_ip):
-            fab.sudo('rm /etc/network/interfaces.d/50-cloud-init.cfg')
+        if static_ip:
             fab.sudo(
-                'echo """%s""" > /etc/network/interfaces.d/%s.cfg' %
-                (eth_interface.format(**AV), AV['eth_iface']))
-            # disable cloud init
-            fab.put(
-                local_path='pi3_cloud.cfg',
-                remote_path='/etc/cloud/cloud.cfg',
-                use_sudo=True,
-                mode='0644')
+                'echo """%s""" >> /etc/dhcpcd.conf' %
+                eth_interface.format(**AV))
         # enable passwordless root login via ssh
+        fab.sudo("""mkdir /root/.ssh""")
+        fab.sudo("""chmod 700 /root/.ssh""")
         fab.put(
             local_path=authorized_keys,
             remote_path='/root/.ssh/authorized_keys',
@@ -54,5 +48,4 @@ def bootstrap(boot_ip=None, authorized_keys='authorized_keys', static_ip=True):
         fab.sudo("""chown root:root /root/.ssh/authorized_keys""")
     fab.sudo("""apt update""")
     fab.sudo("""apt upgrade -y""")
-    fab.sudo("""apt install python2.7-minimal -y""")
     fab.reboot()
