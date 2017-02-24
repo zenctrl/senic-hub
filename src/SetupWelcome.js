7d5f5bc81764129f2b0619304562e08a4cfc55b6
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-02-24 17:35:09 +0100

    Update welcome step style

--- src/SetupWelcome.js
+++ src/SetupWelcome.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router';
+import './SetupWelcome.css'
 
 class SetupWelcome extends Component {
   render() {
     return (
-      <div>
+      <div className="SetupWelcome">
         <p>Welcome to Nuimo Hub. Press "continue" to continue :)</p>
         <Link to="setup/wifi">Continue</Link>
       </div>
