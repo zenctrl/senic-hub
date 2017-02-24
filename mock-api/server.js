330687bcd7caa015b8a7ce1517b78157b5ee67e5
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-02-24 12:07:13 +0100

    Discover mocked nuimo only after 5 seconds

--- mock-api/server.js
+++ mock-api/server.js
@@ -3,6 +3,8 @@ var jsonServer = require('json-server')
 
 routes = JSON.parse(fs.readFileSync('routes.json'))
 
+var firstNuimoSetupRequestDate = null
+
 var server = jsonServer.create()
 server.use(jsonServer.defaults())
 server.use(jsonServer.rewriter(routes))
@@ -10,6 +12,20 @@ server.use(function (request, response, next) {
   // Respond all POST requests with 200 ignoring the request
   if (request.method === 'POST') {
     response.jsonp(request.query)
+  }
+  else {
+    next()
+  }
+})
+server.use(function (request, response, next) {
+  if (request.url === '/setup-nuimo' && request.method === 'GET') {
+    firstNuimoSetupRequestDate = firstNuimoSetupRequestDate || Date.now()
+    if (Date.now() - firstNuimoSetupRequestDate > 5000) {
+      next() // Returns discovered Nuimos
+    }
+    else {
+      response.jsonp([]) // Nuimo not yet discovered
+    }
   }
   else {
     next()
