7935f91126a639315bf55f04044d619ca6af7e2c
Tom Lazar <tomster@pyfidelity.com>
2017-02-28 15:45:55 +0100

    Don't upgrade to raspbian stretch
    It gives us python3.5 but it unfortunately also breaks bluez :(
    
    Instead we install python3.5 from sources

--- deployment/fab_pi3.py
+++ deployment/fab_pi3.py
@@ -26,10 +26,8 @@ def bootstrap(boot_ip=None, authorized_keys='authorized_keys', static_ip=True):
     hostname = env.instance.uid
     env.host_string = 'pi@%s' % hostname
     env.password = 'raspberry'
-    # upgrade to raspbian strech (gives us python3.5)
-    fab.sudo("""echo 'deb http://mirrordirector.raspbian.org/raspbian/ testing main contrib non-free rpi' > /etc/apt/sources.list.d/stretch.list""")
     fab.sudo("""apt update""")
-    fab.sudo("""apt dist-upgrade -y""")
+    fab.sudo("""apt upgrade -y""")
     AV = env.instance.get_ansible_variables()
     AV.setdefault('eth_ip', final_ip)
     AV.setdefault('eth_iface', 'eth0')
