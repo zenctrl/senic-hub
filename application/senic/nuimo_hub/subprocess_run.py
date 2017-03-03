c69d99ff9c102926d94d8bd2d55c5de40f5e2072
Martins Grunskis <martins@grunskis.com>
2017-03-03 16:57:58 +0100

    Remove parameters that are not supported
    Apparently Python 3.4 doesn't have encoding and errors parameters

--- application/senic/nuimo_hub/subprocess_run.py
+++ application/senic/nuimo_hub/subprocess_run.py
@@ -6,6 +6,6 @@ except ImportError:
     from collections import namedtuple
     from subprocess import check_output
     def run(args, *, stdin=None, input=None, stdout=None, stderr=None, shell=False, timeout=None, check=False, encoding=None, errors=None):
-        stdout_bytes = check_output(args, stdin=stdin, stderr=stderr, shell=shell, encoding=encoding, errors=errors, timeout=timeout)
+        stdout_bytes = check_output(args, stdin=stdin, stderr=stderr, shell=shell, timeout=timeout)
         Output = namedtuple('Output', ['stdout'])
         return Output(stdout=stdout_bytes)
