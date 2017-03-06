5c9200cc194997c3972e46d0ddb6c1755d2ff9ed
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-06 11:51:22 +0100

    Clean code

--- application/senic/nuimo_hub/subprocess_run.py
+++ application/senic/nuimo_hub/subprocess_run.py
@@ -5,7 +5,9 @@ try:
 except ImportError:
     from collections import namedtuple
     from subprocess import check_output
-    def run(args, *, stdin=None, input=None, stdout=None, stderr=None, shell=False, timeout=None, check=False, encoding=None, errors=None):
+
+    def run(args, *, stdin=None, input=None, stdout=None, stderr=None, shell=False, timeout=None, check=False,
+            encoding=None, errors=None):
         stdout_bytes = check_output(args, stdin=stdin, stderr=stderr, shell=shell, timeout=timeout)
         Output = namedtuple('Output', ['stdout'])
         return Output(stdout=stdout_bytes)
