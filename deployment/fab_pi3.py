134154b81a76734a17fdf72076b0e93509676156
Tom Lazar <tomster@pyfidelity.com>
2017-03-07 17:41:09 +0100

    Include verbatim copy of rsync helper
    This comes from the bsdploy utility and should ideally be included in ploy
    itself, because it is not bsd specific.
    
    Until then we simply use a copy&paste version :)

--- deployment/fab_pi3.py
+++ deployment/fab_pi3.py
@@ -2,6 +2,8 @@
 from os import path
 from fabric import api as fab
 from fabric.api import task, env
+from ploy.common import shjoin
+
 
 eth_interface = """auto {eth_iface}
 interface {eth_iface}
@@ -49,3 +51,33 @@ def bootstrap(boot_ip=None, authorized_keys='authorized_keys', static_ip=True):
             mode='0700')
         fab.sudo("""chown root:root /root/.ssh/authorized_keys""")
     fab.reboot()
+
+
+@task
+def rsync(*args, **kwargs):
+    """ wrapper around the rsync command.
+        the ssh connection arguments are set automatically.
+        any args are just passed directly to rsync.
+        you can use {host_string} in place of the server.
+        the kwargs are passed on the 'local' fabric command.
+        if not set, 'capture' is set to False.
+        example usage:
+        rsync('-pthrvz', "{host_string}:/some/src/directory", "some/destination/")
+    """
+    kwargs.setdefault('capture', False)
+    replacements = dict(
+        host_string="{user}@{host}".format(
+            user=env.instance.config.get('user', 'root'),
+            host=env.instance.config.get(
+                'host', env.instance.config.get(
+                    'ip', env.instance.uid))))
+    args = [x.format(**replacements) for x in args]
+    ssh_info = env.instance.init_ssh_key()
+    ssh_info.pop('host')
+    ssh_info.pop('user')
+    ssh_args = env.instance.ssh_args_from_info(ssh_info)
+    cmd_parts = ['rsync']
+    cmd_parts.extend(['-e', "ssh %s" % shjoin(ssh_args)])
+    cmd_parts.extend(args)
+    cmd = shjoin(cmd_parts)
+    return fab.local(cmd, **kwargs)
