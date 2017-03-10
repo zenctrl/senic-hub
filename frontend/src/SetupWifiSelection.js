97fdf90be2470a33b37c84a775a74888607d2556
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-10 11:24:54 +0100

    Fix wifi ssid poll timer not stopped on unmount

--- frontend/src/SetupWifiSelection.js
+++ frontend/src/SetupWifiSelection.js
@@ -45,7 +45,7 @@ class SetupWifiSelection extends Component {
 
   pollSsids() {
     //TODO: Promise chain doesn't get cancelled when component unmounts
-    let done = () => setTimeout(this.pollSsids.bind(this), this.ssidPollInterval)
+    let done = () => this.ssidPollTimer = setTimeout(this.pollSsids.bind(this), this.ssidPollInterval)
     fetch('/-/setup/wifi')
       //TODO: Write tests for all possible API call responses, server not available, etc.
       .then((response) => response.json())
