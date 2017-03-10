ebe770256610f4633c2c27861c3ff71afb633d2a
Lars Alexander Blumberg <lars.blumberg@posteo.de>
2017-03-10 11:05:51 +0100

    Adjust ssids response to new format

--- frontend/src/SetupWifiSelection.js
+++ frontend/src/SetupWifiSelection.js
@@ -50,8 +50,6 @@ class SetupWifiSelection extends Component {
       //TODO: Write tests for all possible API call responses, server not available, etc.
       .then((response) => response.json())
       .then((ssids) => {
-        //TODO: As soon as backend only returns array, remove following line
-        ssids = Object.keys(ssids)
         ssids = ssids.sort((lhs, rhs) => lhs.toLowerCase().localeCompare(rhs.toLowerCase()))
         this.setState({ssids: ssids})
         done()
