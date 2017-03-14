1279c3a49515fc3d941b6f940ac26a7a2b17cbe3
Tom Lazar <tomster@pyfidelity.com>
2017-03-14 18:08:51 +0100

    Upload devpi
    When tests succeed for master, we upload it to the master index

---
+++ application/travis_after_success.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# "piggyback" on the existing tox environment
+DEVPI=./.tox/py35/bin/devpi
+
+if [ "$TRAVIS_BRANCH" = "master" ] ; then
+    $DEVPI use https://pypi.senic.com/$devpi_index
+    $DEVPI login $devpi_user --password="$devpi_password"
+    $DEVPI upload --no-vcs --with-docs --formats bdist_wheel
+else
+    echo "Not building for $TRAVIS_BRANCH"
+fi
