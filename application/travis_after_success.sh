1a206f1ece7efaf218c1b15f6f19933ba8435c01
Tom Lazar <tomster@pyfidelity.com>
2017-03-14 18:26:34 +0100

    Don't build for PR runs

--- application/travis_after_success.sh
+++ application/travis_after_success.sh
@@ -3,7 +3,7 @@
 # "piggyback" on the existing tox environment
 DEVPI=./.tox/py35/bin/devpi
 
-if [ "$TRAVIS_BRANCH" = "master" ] ; then
+if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
     $DEVPI use https://pypi.senic.com/$devpi_index
     $DEVPI login $devpi_user --password="$devpi_password"
     $DEVPI upload --no-vcs --with-docs --formats bdist_wheel
