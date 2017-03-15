bb5eea8c2846fd6970bc264f45a6952e5d0c721f
Tom Lazar <tomster@pyfidelity.com>
2017-03-15 14:10:37 +0100

    We no longer need to install the development dependencies.
    They are now part of the tox venv.

--- application/travis_after_success.sh
+++ application/travis_after_success.sh
@@ -2,11 +2,9 @@
 
 # "piggyback" on the existing tox environment
 DEVPI="$TRAVIS_BUILD_DIR/application/.tox/py35/bin/devpi"
-PIP="/home/travis/virtualenv/python3.5.2/bin/pip"
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
-    $PIP install senic.nuimo_hub[development]
-    $DEVPI use https://pypi.senic.com/${TRAVIS_REPO_SLUG%/*}/master
+    $DEVPI use "https://pypi.senic.com/${TRAVIS_REPO_SLUG%/*}/master"
     $DEVPI login $devpi_user --password="$devpi_password"
     $DEVPI upload --no-vcs --with-docs --formats bdist_wheel
 else
