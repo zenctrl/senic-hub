a576fb4a1a70367079c1d4bd5e13f38a9e2fddcd
Tom Lazar <tomster@pyfidelity.com>
2017-03-22 14:25:52 +0100

    Adjust sphinx setup to new naming

--- docs/conf.py
+++ docs/conf.py
@@ -20,4 +20,4 @@ autodoc_member_order = 'bysource'
 # general substitutions
 project = 'Nuimo Hub'
 copyright = '2016'
-version = release = get_distribution('senic_hub.backend').version
+version = release = get_distribution('senic_hub').version
