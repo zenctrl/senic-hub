8123a2bbb72e5586a49a5e71d48939997f3e2df8
Neelotpal Nag <neel@senic.com>
2017-12-06 11:56:54 +0100

    Fix PEP8 Compliance

--- senic_hub/backend/views/nuimos.py
+++ senic_hub/backend/views/nuimos.py
@@ -211,9 +211,8 @@ def delete_nuimo(request):  # pragma: no cover,
                 supervisor.restart_program('nuimo_app')
         except Exception as e:
             # SUpervior spits out errors when two nuimos are being removed before the supervisor
-            # restarts post first-nuimo-removal.  
+            # restarts post first-nuimo-removal.
             logger.error(str(e))
-
 
 
 # TODO: if required, due to performance issues, can be implemented in a separate endpoint
