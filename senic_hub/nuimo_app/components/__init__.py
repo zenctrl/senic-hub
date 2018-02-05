2106c6ba76b5686b28b37878587e235f66ac7d79
Avichal <pandeyavichal7@gmail.com>
2018-02-05 12:52:54 +0530

    remove obvious unused stuff

--- senic_hub/nuimo_app/components/__init__.py
+++ senic_hub/nuimo_app/components/__init__.py
@@ -1,8 +1,7 @@
 import logging
-from pprint import pformat
+
 from threading import Thread
 from .. import matrices
-from ..hass import HomeAssistant
 
 
 logger = logging.getLogger(__name__)
@@ -64,62 +63,3 @@ class ThreadComponent(BaseComponent):
         """
         raise NotImplementedError()
 
-
-class HomeAssistantComponent(BaseComponent):
-    def __init__(self, ha_domain, component_config):
-        super().__init__(component_config)
-
-        self.is_on = False
-
-        # TODO: Parametrize HA's address?
-        self._ha_address = "localhost:8123"
-        self._ha_entity_id = component_config['ha_entity_id']
-        self._ha_domain = ha_domain
-
-    def start(self):
-        super().start()
-
-        # TODO: Provide single HA instance to all HA-based components
-        self._ha = HomeAssistant(self._ha_address, on_connect=self._ha_connected, on_disconnect=self._ha_disconnected)
-        self._ha.start()
-        self._ha.register_state_listener(self._ha_entity_id, self._ha_state_changed)
-
-    def stop(self):
-        super().stop()
-        self._ha.stop()
-        self._ha.unregister_state_listener(self._ha_entity_id)
-
-    def run(self):
-        pass
-
-    def update_from_ha_state(self, state):
-        self.is_on = state.get('state', None) != 'off'
-
-    def call_ha_service(self, service, data={}, on_success=None, on_error=None):
-        def _on_success(result):
-            logger.debug("Calling service %s:%s succeeded with result: %s", self._ha_domain, service, pformat(result))
-
-        def _on_error():
-            logger.debug("Failed calling service %s:%s", self._ha_domain, service)
-
-        data["entity_id"] = self._ha_entity_id
-        logger.debug("Call service %s:%s with data: %s", self._ha_domain, service, pformat(data))
-        self._ha.call_service(self._ha_domain, service, data, on_success or _on_success, on_error or _on_error)
-
-    def _ha_connected(self):
-        def on_state_retrieved(state):
-            self._ha_state_changed(state)
-
-        def on_state_retrieve_failed():
-            logger.debug("HA get state failed")
-
-        self._ha.get_state(self._ha_entity_id, on_state_retrieved, on_state_retrieve_failed)
-
-    def _ha_disconnected(self):
-        pass
-
-    def _ha_state_changed(self, state):
-        if "data" in state:
-            self.update_from_ha_state(state["data"]["new_state"])
-        else:
-            self.update_from_ha_state(state)
